1e2189962c59537d9d096e28e0144b3415470cdd
Lorenzo <lorenzo.la-mura@alumni.univ-avignon.fr>
2022-05-04 21:39:09 +0200

    Add Test for IPokedexFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,15 +1,20 @@
 package fr.univavignon.pokedex.api;
 
-import junit.framework.TestCase;
+import org.mockito.Mockito;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
-public class IPokedexFactoryTest extends TestCase {
+public class IPokedexFactoryTest  {
 
-	public IPokedexFactoryTest(String name) {
-		super(name);
-	}
-	
-	public void testApp() {
-		assertTrue(true);
+	@Test
+	public void testCreatePokedex() {
+		IPokedexFactory iPokedexFactory = Mockito.mock(IPokedexFactory.class);
+		IPokemonMetadataProvider metadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+		IPokemonFactory pokemonFactory = Mockito.mock(IPokemonFactory.class);
+		IPokedex ipokedex = Mockito.mock(IPokedex.class);
+		when(iPokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(ipokedex);
+		assertEquals(iPokedexFactory.createPokedex(metadataProvider, pokemonFactory), ipokedex);
 	}
 
 }
