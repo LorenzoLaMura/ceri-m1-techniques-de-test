b8a4987551486de7ff8f92866fae79e6feb7038c
Lorenzo <lorenzo.la-mura@alumni.univ-avignon.fr>
2022-05-04 21:32:30 +0200

    Add Test for IPokemonFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,15 +1,26 @@
 package fr.univavignon.pokedex.api;
 
-import junit.framework.TestCase;
+import org.mockito.Mockito;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
-public class IPokemonFactoryTest extends TestCase {
+public class IPokemonFactoryTest{
 
-	public IPokemonFactoryTest(String name) {
-		super(name);
-	}
-	
-	public void testApp() {
-		assertTrue(true);
+	@Test
+	public void testCreatePokemon() {
+		IPokemonFactory iPokemonFactoryMock = Mockito.mock(IPokemonFactory.class);
+		Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+		when(iPokemonFactoryMock.createPokemon(anyInt(),eq(613),eq(64),eq(4000),eq(4))).thenAnswer(input -> {
+			if((int)input.getArgument(0) > 150 || (int)input.getArgument(0) < 0) throw new PokedexException("Index must be between 0 and 150!");
+			else return pokemon; 
+		});
+		assertThrows(PokedexException.class,()->iPokemonFactoryMock.createPokemon(-1,613,64,4000,4));
+		assertThrows(PokedexException.class,()->iPokemonFactoryMock.createPokemon(151,613,64,4000,4));
+		assertEquals(pokemon, iPokemonFactoryMock.createPokemon(0, 613, 64, 4000, 4));
 	}
 
 }
