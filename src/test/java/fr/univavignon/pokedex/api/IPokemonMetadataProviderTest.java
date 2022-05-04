5237a28d8dadb28a3ec726be7cf0b73d0842b9ec
Lorenzo <lorenzo.la-mura@alumni.univ-avignon.fr>
2022-05-04 21:46:10 +0200

    Add Test for IPokemonMetadataProvider

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,15 +1,25 @@
 package fr.univavignon.pokedex.api;
 
-import junit.framework.TestCase;
+import org.mockito.Mockito;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
-public class IPokemonMetadataProviderTest extends TestCase {
+public class IPokemonMetadataProviderTest{
 
-	public IPokemonMetadataProviderTest(String name) {
-		super(name);
-	}
-	
-	public void testApp() {
-		assertTrue(true);
+	@Test
+	public void testGetPokemonMetadata() throws PokedexException {
+		IPokemonMetadataProvider iPokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+		PokemonMetadata pokemonMetadata = Mockito.mock(PokemonMetadata.class);
+		when(iPokemonMetadataProvider.getPokemonMetadata(anyInt())).thenAnswer(input -> {
+			if((int)input.getArgument(0) > 150 || (int)input.getArgument(0) < 0) throw new PokedexException("Index must be between 0 and 150!");
+			else return pokemonMetadata; 
+		});
+		assertThrows(PokedexException.class,()->iPokemonMetadataProvider.getPokemonMetadata(-1));
+		assertThrows(PokedexException.class,()->iPokemonMetadataProvider.getPokemonMetadata(151));
+		assertEquals(iPokemonMetadataProvider.getPokemonMetadata(0), pokemonMetadata);
 	}
 
 }
