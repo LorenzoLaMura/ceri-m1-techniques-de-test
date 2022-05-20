35a7eed8000c520da0317c67c3b452804e8a614f
Lorenzo <lorenzo.la-mura@alumni.univ-avignon.fr>
2022-05-20 18:19:33 +0200

    TP4 - Implementation of the Tests of the new Classes

---
+++ src/test/java/fr/univavignon/pokedex/api/PokemonTrainerFactoryTest.java
@@ -0,0 +1,17 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class PokemonTrainerFactoryTest {
+
+	@Test
+	public void testCreateTrainer() {
+		PokemonTrainerFactory pokemonTrainerFactory = new PokemonTrainerFactory();
+		PokemonTrainer trainer = pokemonTrainerFactory.createTrainer("Lorenzo", Team.INSTINCT, new PokedexFactory());
+		PokemonTrainer trainer2 = new PokemonTrainer("Lorenzo",Team.INSTINCT,new Pokedex(new PokemonMetadataProvider(),new PokemonFactory()));
+		assertEquals(trainer.getName(),trainer2.getName());
+		assertEquals(trainer.getTeam(),trainer2.getTeam());
+	}
+
+}
