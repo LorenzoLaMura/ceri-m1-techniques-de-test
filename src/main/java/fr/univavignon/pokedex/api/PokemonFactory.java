f080bbbfe6f036e3f74d5f1f2d952dfd4585e952
Lorenzo <lorenzo.la-mura@alumni.univ-avignon.fr>
2022-05-20 18:11:00 +0200

    Fix PokemonFactory

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -4,10 +4,9 @@ public class PokemonFactory implements IPokemonFactory{
 
 	@Override
 	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-		new Pokemon(index, Pokedex.pokemons.get(index).getName(), Pokedex.pokemons.get(index).getAttack(), 
+		return new Pokemon(index, Pokedex.pokemons.get(index).getName(), Pokedex.pokemons.get(index).getAttack(), 
 				Pokedex.pokemons.get(index).getDefense(), Pokedex.pokemons.get(index).getStamina(), 
 				cp,hp, dust, candy, Pokedex.pokemons.get(index).getIv());
-		return null;
 	}
 
 } 
